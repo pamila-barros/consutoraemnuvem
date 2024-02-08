110620c20bbc3c0f1de018eb6a6036cb57e3388c
Pamila Barros <56457900+pamila-barros@users.noreply.github.com>
2024-02-08 01:34:51 +0000

    config: addind pylint to project

---
+++ example.py
@@ -0,0 +1,9 @@
+def my_func():
+  print('Olá')
+
+    # Pyliny
+    # snake_case valida nomes de funcoes, variaveis, metodos
+    # PascalCase -> classes de orientacoes a objetos
+    
+    # elementos de importancia
+  
